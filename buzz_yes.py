6afbf31881af60e770d56ff0b1e39f1e0b587670
saisd138 <89975889+saisd138@users.noreply.github.com>
2024-01-31 17:28:15 +0530

    used buzzer

--- buzz_yes.py
+++ buzz_yes.py
@@ -1,10 +1,12 @@
+from gpiozero import Buzzer
 from time import sleep
-from gpiozero import LED
 
-buzz = LED (26)
+# Assuming the buzzer is connected to GPIO pin 26
+buzzer_pin = 26
+buzzer = Buzzer(buzzer_pin)
 
-
- buzz.on ()
- sleep (0.3)
- buzz.off()
- sleep(.3)
+try:
+    buzzer.on()  # Turn the buzzer on
+    sleep(0.1)     # Beep for 1 second (adjust the duration as needed)
+finally:
+    buzzer.off()

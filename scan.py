5b82b1c48d757678e40e5046f3c3b7643ffacfe3
saisd138 <89975889+saisd138@users.noreply.github.com>
2024-01-31 12:20:31 +0530

    rfid read file scan.py

---
+++ scan.py
@@ -0,0 +1,26 @@
+#sudo apt-get update
+#sudo apt-get install python3 python3-pip
+#sudo pip3 install nfcpy
+
+
+
+import nfc
+
+def on_connect(tag):
+    uid = tag.identifier.hex()
+    print(f'UID: {uid}')
+    return True  # Set the flag to exit the loop
+
+clf = nfc.ContactlessFrontend('tty:S0')  # Use 'tty:AMA0' or other port based on your configuration
+
+try:
+    card_read = False  # Flag to indicate whether a card has been read
+
+    # Use an event-driven approach
+    clf.connect(rdwr={'on-connect': on_connect})
+
+except KeyboardInterrupt:
+    pass  # Handle Ctrl+C gracefully
+
+finally:
+    clf.close()

bca1ab9c3a202b5774636e11262d22ec2e9d51d6
saisd138 <89975889+saisd138@users.noreply.github.com>
2024-01-31 17:27:20 +0530

    beep 5 times

--- buzz_no.py
+++ buzz_no.py
@@ -1,18 +1,15 @@
+from gpiozero import Buzzer
 from time import sleep
-from gpiozero import LED
 
-buzz = LED (26)
+# Assuming the buzzer is connected to GPIO pin 26
+buzzer_pin = 26
+buzzer = Buzzer(buzzer_pin)
 
-
- buzz.on ()
- sleep (0.3)
- buzz.off()
- sleep(.2)
- buzz.on ()
- sleep (0.3)
- buzz.off()
- sleep(.2)
- buzz.on ()
- sleep (0.3)
- buzz.off()
- sleep(.2)
+try:
+    for _ in range(5):  # Beep thrice
+        buzzer.on()      # Turn the buzzer on
+        sleep(0.3)       # On delay of 0.5 seconds
+        buzzer.off()     # Turn the buzzer off
+        sleep(0.1)       # Off delay of 0.2 seconds
+except KeyboardInterrupt:
+    pass  # Handle keyboard interrupt if needed
